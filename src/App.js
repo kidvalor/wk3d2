60dfd406a421f0b8ba14b3c5afb483236f03f425
kidvalor <87155806+kidvalor@users.noreply.github.com>
2021-09-13 21:51:24 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,132 +1,76 @@
-import Student from "./Student";
-import './App.css';
-import { useState } from "react"
+import styled from "styled-components";
+import { useState } from "react";
+import CityComponent from "./modules/City"
+import WeatherComponent from "./modules/WeatherInfo";
+import Axios from "axios";
+
+export const WeatherIcons = {
+    "01d": "/react-weather-app/icons/sunny.svg",
+    "01n": "/react-weather-app/icons/night.svg",
+    "02d": "/react-weather-app/icons/day.svg",
+    "02n": "/react-weather-app/icons/cloudy-night.svg",
+    "03d": "/react-weather-app/icons/cloudy.svg",
+    "03n": "/react-weather-app/icons/cloudy.svg",
+    "04d": "/react-weather-app/icons/perfect-day.svg",
+    "04n": "/react-weather-app/icons/cloudy-night.svg",
+    "09d": "/react-weather-app/icons/rain.svg",
+    "09n": "/react-weather-app/icons/rain-night.svg",
+    "10d": "/react-weather-app/icons/rain.svg",
+    "10n": "/react-weather-app/icons/rain-night.svg",
+    "11d": "/react-weather-app/icons/storm.svg",
+    "11n": "/react-weather-app/icons/storm.svg",
+};
+
+const API_KEY = "8554c5268084fdcdf6b9d9ac68fb4389"
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin: auto;
+align-items: center;
+box-shadow: 0 3px 6px 0 #555;
+padding: 20px 10px;
+border-radius: 4px;
+width: 380px;
+background: white;
+`;
+
+const City = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const Weather = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const AppLabel = styled.span`
+color: black;
+font-size: 18px;
+front-weight: bold; 
+`;
+
 
 function App() {
 
-const [studentData, setStudentData] = useState({
-  students: [
-    {
-
-      name: 'Cait Yomorta',
-      bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum',
-        scores: [
-          {
-  
-            date: '2018-02-08',
-            score: 77
-  
-          },
-  
-          {
-  
-          date: '2018-04-22',
-          score: 92
-  
-          },
-  
-          {
-  
-          date: '2018-09-15',
-          score: 68
-  
-          }
-  
-              ]
-  
-    },
-  
-    {
-  
-      name: 'Holly Baird',
-  
-      bio: 'Eum molestiae explicabo deserunt, maiores quod eaque omnis tenetur vero ducimus, magnam autem! Quia facere quaerat eum repudiandae dolorum eligendi iure quae. Eos id possimus accusantium, earum animi modi hic.',
-  
-      scores: [
-  
-        {
-  
-          date: '2018-12-14',
-  
-          score: 88
-  
-        },
-  
-        {
-  
-          date: '2019-01-09',
-  
-          score: 79
-  
-        },
-  
-        {
-  
-          date: '2019-02-23',
-  
-          score: 91
-  
-        },
-  
-        {
-  
-          date: '2019-03-01',
-  
-          score: 95
-  
-        }
-  
-      ]
-  
-    },
-  
-    {
-  
-      name: 'Wes Mungia',
-  
-      bio: 'Repudiandae veritatis recusandae quidem tenetur impedit, numquam incidunt enim, adipisci id cupiditate asperiores nam perferendis. Facere odit laborum ipsum autem repellendus natus eius doloremque ullam perferendis. Enim repellendus ut veniam?',
-  
-      scores: [
-  
-        {
-  
-          date: '2018-10-11',
-  
-          score: 62
-  
-        },
-  
-        {
-  
-          date: '2018-11-24',
-  
-          score: 74
-  
-        },
-  
-        {
-  
-          date: '2018-12-19',
-  
-          score: 85
-  
-        }
-  
-      ]
-  
-    }
-  ]
-
-})
-
-
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,);
+    updateWeather(response.data);
+  };
   return (
-    <div id="student-data">
-      <h1 className="stu-data">Student Data</h1>
-  {studentData.students.map((student, index) => (
-  <Student key={index} student={student} />))
-}
-  </div>
+    
+    <Container>
+    {city && weather ? (
+      <WeatherComponent weather={weather} city={city} />
+    ) : (
+      <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+    )}
+  </Container>
+
   );
 }
 
